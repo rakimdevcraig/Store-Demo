177588213c9f41dc01625d0e9c62ec8cd87065e9
rakimdevcraig <rakimdevcraig@gmail.com>
2020-08-07 10:47:52 -0400

    made client folder a static folder

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require("path")
+
 
 const app = express();
 
@@ -23,9 +25,14 @@ mongoose
     .then(() => console.log('MongoDB Connected.....'))
     .catch(err => console.log(err))
 
+//set a static folder if we want to serve multiple html pages, & files like css/js
+app.use(express.static(path.join(__dirname, 'client')))
+
+
 //use routes
 //anything that refers to api/items use the items variable uptop
 app.use('/api/items', items);
+
 
 const PORT = process.env.PORT || 5000;
 
